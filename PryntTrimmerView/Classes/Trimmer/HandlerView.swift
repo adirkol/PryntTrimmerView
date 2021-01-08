d47e30a56268400e6115e2e7a95e3ae6a3d01355
Adir Kol <koladir@gmail.com>
2021-01-08 14:30:28 +0200

    Amit fix

--- PryntTrimmerView/Classes/Trimmer/HandlerView.swift
+++ PryntTrimmerView/Classes/Trimmer/HandlerView.swift
@@ -12,11 +12,11 @@ import UIKit
 class HandlerView: UIView {
 
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        let hitFrame = bounds.insetBy(dx: -40, dy: -20)
+        let hitFrame = bounds.insetBy(dx: -20, dy: -20)
         return hitFrame.contains(point) ? self : nil
     }
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
-        let hitFrame = bounds.insetBy(dx: -40, dy: -20)
+        let hitFrame = bounds.insetBy(dx: -20, dy: -20)
         return hitFrame.contains(point)
     }
 }
